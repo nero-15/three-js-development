15eb086624c2e6a0adf8c51f9fcb004f3a582d98
nero-15 <ti7805@gmail.com>
2021-04-12 23:03:01 +0900

    setting

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
+	mode: 'development',
 	devtool: 'inline-source-map',
 	entry: './app.js',
 	output: {
-		filename: 'bundle.js'
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
 	},
 	resolve: {
 	},
