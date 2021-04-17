81f178b69abd48dc65adf0d3caca540e3aaa6300
nero-15 <ti7805@gmail.com>
2021-04-17 23:56:17 +0900

    Drawing lines

--- src/app.js
+++ src/app.js
@@ -1,30 +1,48 @@
-var scene, camera, renderer, geometry, material, cube;
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-init();
-animate();
+line()
+//cube();
 
-function init() {
+function line() {
+	const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+	camera.position.set(0, 0, 100);
+	camera.lookAt(0, 0, 0);
 
-	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	document.body.appendChild(renderer.domElement);
+	const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
-	geometry = new THREE.BoxGeometry();
-	material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-	cube = new THREE.Mesh(geometry, material);
+	const points = [];
+	points.push(new THREE.Vector3(- 10, 0, 0));
+	points.push(new THREE.Vector3(0, 10, 0));
+	points.push(new THREE.Vector3(10, 0, 0));
+
+	const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+	const line = new THREE.Line(geometry, material);
+
+	scene.add(line);
+	renderer.render(scene, camera);
+}
+
+function cube() {
+	const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	const geometry = new THREE.BoxGeometry();
+	const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+	const cube = new THREE.Mesh(geometry, material);
 	scene.add(cube);
 
 	camera.position.z = 5;
 
-}
+	const animate = function() {
+		requestAnimationFrame(animate);
 
-function animate() {
-	requestAnimationFrame(animate);
+		cube.rotation.x += 0.01;
+		cube.rotation.y += 0.01;
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+		renderer.render(scene, camera);
+	};
+	animate();
 
-	renderer.render(scene, camera);
 }
